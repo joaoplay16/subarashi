e97d15d6b8e20288d6ae52d74c24b33d4b41f2b6
joao <joaoplay16@gmail.com>
2020-07-08 12:36:19 -0300

    useUnifiedTopology removed

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-if(true){
+if(process.env.NODE_ENV == 'production'){
 	module.exports = {mongoURI: 'mongodb+srv://subarashi:Ny.L00K5@cluster0.gsnq4.gcp.mongodb.net/subarashi?retryWrites=true&w=majority'}
 }else{
 	module.exports = {mongoURI: 'mongodb://localhost/subarashi'}
